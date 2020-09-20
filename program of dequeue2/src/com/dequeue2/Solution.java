e71f5da534ec04d1f4f308e23ede01aa0d16b519
raman <raman@192.168.0.8>
2020-09-20 21:59:22 +0530

    Deque in java.

---
+++ program of dequeue2/src/com/dequeue2/Solution.java
@@ -0,0 +1,25 @@
+package com.dequeue2;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+public static void main(String[] args) {
+Deque<String> deque=new ArrayDeque<String>();  
+deque.offer("arvind");  
+deque.offer("vimal");  
+deque.add("mukul");  
+deque.offerFirst("jai");  
+System.out.println("After offerFirst Traversal...");  
+for(String s:deque){  
+System.out.println(s);  
+}  
+//deque.poll();  
+//deque.pollFirst();//it is same as poll()  
+deque.pollLast();  
+System.out.println("After pollLast() Traversal...");  
+for(String s:deque){  
+System.out.println(s);  
+}  
+}
+}
