c35d354cac9562b0c1d33c3a3d694be4887e6a37
raman <raman@192.168.0.8>
2020-09-20 20:18:13 +0530

    Finding Permutation in java.

---
+++ find permutation/src/com/npr/Permutation.java
@@ -0,0 +1,27 @@
+package com.npr;
+
+import java.util.Scanner;
+
+public class Permutation {
+public static int NpR(int n,int r)
+{
+return fact(n)/fact(n-r);	
+}
+public static int fact(int n)
+{
+int res=1;
+for(int i=2;i<=n;i++)
+res *=i;
+return res;
+}
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);	
+System.out.println("Enter the value of n and r");
+int n=sc.nextInt();
+int r=sc.nextInt();
+System.out.println("Permutation of given number: "+NpR(n, r));
+
+
+	}
+
+}
