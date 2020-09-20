356db0dd6a47feab6a587be29f098eba05116540
raman <raman@192.168.0.8>
2020-09-20 22:19:05 +0530

    Queue in Java.

---
+++ program of queue/src/com/queue/Solution.java
@@ -0,0 +1,27 @@
+package com.queue;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+public class Solution {
+public static void main(String[] args) {
+Queue<String> queue=new PriorityQueue<String>();  
+queue.add("Amit Sharma");  
+queue.add("Vijay Raj");  
+queue.add("JaiShankar");  
+queue.add("Raj");  
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr=queue.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+queue.remove();  
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr2=queue.iterator();  
+while(itr2.hasNext()){  
+System.out.println(itr2.next());  
+}
+}
+}
